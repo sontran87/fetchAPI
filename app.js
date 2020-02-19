57a868cd744cf0171caff7bc44199c68cbd114d2
sontran87 <transon166@gmail.com>
2020-02-19 09:42:17 +0700

    Refactor with arrow function

--- app.js
+++ app.js
@@ -5,23 +5,17 @@ document.getElementById('button3').addEventListener('click', getExteral);
 // Get data from local text file
 function getText() {
   fetch('test.txt')
-    .then(function(res) {
-      return res.text();
-    })
+    .then(res => res.text())
     .then(function(result) {
       document.getElementById('output').innerHTML = result;
     })
-    .catch(function(err) {
-      console.log(`Error: ${err} Some thing went wrong`);
-    });
+    .catch(err => console.log(`Error: ${err} Some thing went wrong`));
 }
 
 // Get data from local JSON file
 function getJson() {
   fetch('sample.json')
-    .then(function(res) {
-      return res.json();
-    })
+    .then(res => res.json())
     .then(function(post) {
       let output = '';
       post.forEach(function(item) {
@@ -29,17 +23,13 @@ function getJson() {
       });
       document.getElementById('output').innerHTML = output;
     })
-    .catch(function(err) {
-      console.log(`Error: ${err} Some thing went wrong`);
-    });
+    .catch(err => console.log(`Error: ${err} Some thing went wrong`));
 }
 
 // Get data from exteral API
 function getExteral() {
   fetch('https://api.github.com/users')
-    .then(function(res) {
-      return res.json();
-    })
+    .then(res => res.json())
     .then(function(users) {
       let output = '';
       users.forEach(function(user) {
@@ -47,7 +37,5 @@ function getExteral() {
       });
       document.getElementById('output').innerHTML = output;
     })
-    .catch(function(err) {
-      console.log(`Error: ${err} Some thing went wrong`);
-    });
+    .catch(err => console.log(`Error: ${err} Some thing went wrong`));
 }
